60b6a9716c0e7aa020297e06ffbf73c74322fc46
Atulya Miglani <atulyamiglani02@gmail.com>
2021-05-12 18:18:06 -0400

    add signup feature on the backend

--- twitter-clone-api/handlers/auth.js
+++ twitter-clone-api/handlers/auth.js
@@ -1,7 +1,49 @@
 const jwt = require("jsonwebtoken"); 
 const db = require('../models/'); 
 
-exports.signin = async function(req, res, next) {}; 
+exports.signin = async function(req, res, next) {
+    try {
+        // find the user 
+        let user = await db.User.findOne({
+            email : req.body.email 
+        });
+
+        let {email, username, profileImageUrl} = user; 
+        let isMatch = await user.comparePassword(req.body.password); 
+        if (isMatch) {
+            let token = jwt.sign({
+                email, 
+                username, 
+                profileImageUrl
+            }, 
+            process.env.SECRET_KEY
+            ); 
+            return res.status(200).json({
+                email, 
+                username, 
+                profileImageUrl, 
+                token
+            })
+        }
+        else {
+            return next({
+                status: 400,
+                message: 'Invalid email/ password'
+            })
+        }
+        // check if the password matches
+        // create a token if it does and return the token
+
+    }catch(error) {
+        return next({
+            status: 400,
+            message: 'something went wrong'
+        })
+        // else send an error in next parameter
+        
+
+    }
+}; 
 
 exports.signup = async function(req, res, next) {
     try {
