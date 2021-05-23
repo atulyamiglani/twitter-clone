a4a9b415566fbfe33bf641ded41953aad31a8f3d
Atulya Miglani <atulyamiglani02@gmail.com>
2021-05-23 15:34:42 -0400

    solve the bugs in signin and signup functions.

--- twitter-clone-api/handlers/auth.js
+++ twitter-clone-api/handlers/auth.js
@@ -1,83 +1,81 @@
-const jwt = require("jsonwebtoken"); 
-const db = require('../models/'); 
+const jwt = require("jsonwebtoken");
+const db = require("../models/");
 
-exports.signin = async function(req, res, next) {
-    try {
-        // find the user 
-        let user = await db.User.findOne({
-            email : req.body.email 
-        });
-
-        let {email, username, profileImageUrl} = user; 
-        let isMatch = await user.comparePassword(req.body.password); 
-        if (isMatch) {
-            let token = jwt.sign({
-                email, 
-                username, 
-                profileImageUrl
-            }, 
-            process.env.SECRET_KEY
-            ); 
-            return res.status(200).json({
-                email, 
-                username, 
-                profileImageUrl, 
-                token
-            })
-        }
-        else {
-            return next({
-                status: 400,
-                message: 'Invalid email/ password'
-            })
-        }
-        // check if the password matches
-        // create a token if it does and return the token
-
-    }catch(error) {
-        return next({
-            status: 400,
-            message: 'something went wrong'
-        })
-        // else send an error in next parameter
-        
-    }
-}; 
-
-exports.signup = async function(req, res, next) {
-    try {
-        // create a user 
-    let user = await db.User.create(req.body); 
-    let {id , username, profileImageUrl} = user; 
-    // create a token
-    let token = jwt.sign({
-        id,
-        username, 
-        profileImageUrl
-    }, process.env.SECRET_KEY); 
-    
-    return res.status(200).json({
-        id, 
-        username, 
-        profileImageUrl, 
-        token
+exports.signin = async function (req, res, next) {
+  try {
+    // find the user
+    let user = await db.User.findOne({
+      email: req.body.email,
     });
 
-    }catch (error) {
-        if (error.code === 11000) {
-            error.message = "sorry! the username or email is wrong!";
-        }
-        return next({
-            status : 400, 
-            message: error.message
-        });
-
+    let { email, username, profileImageUrl } = user;
+    let isMatch = await user.comparePassword(req.body.password);
+    console.log(isMatch);
+    if (isMatch) {
+      let token = jwt.sign(
+        {
+          email,
+          username,
+          profileImageUrl,
+        },
+        process.env.SECRET_KEY
+      );
+      return res.status(200).json({
+        email,
+        username,
+        profileImageUrl,
+        token,
+      });
+    } else {
+      return next({
+        status: 400,
+        message: "Invalid email/ password",
+      });
     }
-    
+    // check if the password matches
+    // create a token if it does and return the token
+  } catch (error) {
+    return next({
+      status: 400,
+      message: "something went wrong",
+    });
+    // else send an error in next parameter
+  }
+};
 
-    
+exports.signup = async function (req, res, next) {
+  try {
+    // create a user
 
-    
+    console.log("pre create", req.body);
+    let user = await db.User.create(req.body);
+    console.log("post create", user);
 
+    let { id, username, profileImageUrl } = user;
 
-}
+    // create a token
+    let token = jwt.sign(
+      {
+        id,
+        username,
+        profileImageUrl,
+      },
+      process.env.SECRET_KEY
+    );
+
+    return res.status(200).json({
+      id,
+      username,
+      profileImageUrl,
+      token,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      error.message = "sorry! the username or email is wrong!";
+    }
+    return next({
+      status: 400,
+      message: error.message,
+    });
+  }
+};
--- twitter-clone-api/models/user.js
+++ twitter-clone-api/models/user.js
@@ -1,60 +1,56 @@
-const mongoose = require('mongoose'); 
-const bcrypt = require('bcrypt'); 
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema( {
-    email : {
-        type: String, 
-        required : true, 
-        unique: true
-    }, 
-    username: {
-        type: String, 
-        required: true, 
-        unique: true
-    }, 
-    password: {
-        type: String,
-        required: true
-    }, 
-    profileImageUrl: {
-        type: String, 
-    }, 
-    messages: [{
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Message'
-    }]
-    
-}); 
-
-// hashing the password before saving it --- 
-userSchema.pre("save", async function(next) {
-    try {
-        if (! this.isModified == "password") {
-            return next();
-        }
-        let hashedPassword = await bcrypt.hash(this.password, 10); 
-        this.password = hashedPassword;
-        return next();
-    
-        }
-
-    catch(err) {
-        return next(err);
-    }
-    
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profileImageUrl: {
+    type: String,
+  },
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  ],
 });
 
-userSchema.method.comparePassword = async function(userPassword, next) {
-    try {
-        let isMatch = await bcrypt.comparePassword(this.password, userPassword);
-        return isMatch;
-
-    }catch (err) {
-        next(err); 
+// hashing the password before saving it ---
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified === "password") {
+      return next();
     }
-}
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
 
+userSchema.methods.comparePassword = async function (userPassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(userPassword, this.password, next);
+    return isMatch;
+  } catch (err) {
+    next(err);
+    console.log(err.message);
+  }
+};
 
-const User = mongoose.model("User", userSchema); 
+const User = mongoose.model("User", userSchema);
 
-module.exports = User; 
+module.exports = User;
